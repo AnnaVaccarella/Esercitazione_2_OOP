19be722ea2ab99e236afb44855eea38685f3ec91
unknown <s283814@studenti.polito.it>
2025-03-26 19:08:50 +0100

    creazione costruttori

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cmath>
+#include <cmath> // per funzioni matematiche 
 #include <concepts>
 
 template<typename T>
@@ -11,4 +11,17 @@ class complex_number {
 	T real;
 	T imag;
 
+public:
+	//Costruttore di default che inizializza la parte reale e immaginaria a 0
+	complex_nuber() : real(0), imag(0) {}
+	//Costruttore user-defined che permette di specificare la parte reale e immaginaria al momento dell'istanza
+	complex_number(T realPart, T imagPart) : real(realPart), imag(imagPart) {}
+	//Modalità per ottenere la parte reale 
+	T getReal() const { return real; }
+	//Modalità per ottenere la parte immaginaria
+	T getImag() const {return imag; }
+	//Modalità per ottenere il coniugato
+	complex_number conjugate() const {
+		return complex_number(real, -imag);
+	}
 	
