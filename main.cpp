6f6474e05d88de2f0199c8501480b80516f5aaf6
unknown <s283814@studenti.polito.it>
2025-03-31 12:10:27 +0200

    costruttori

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
-#include <cmath>
 #include <concepts>
 
-template<typename T>
-concept FloatingPoint = std ::is_floating_point<T>::value;
+template <typename T>
+concept FloatingPoint = std::is_floating_point_v<T>;
 
 template <FloatingPoint T>
 class complex_number {
-	private:
-	T real;
-	T imag;
+private:
+    T real, imag;
 
-	
+public:
+    // Costruttore di default che inizializza la parte reale e immaginaria a 0
+    complex_number() : real(0), imag(0) {}
+
+    // Costruttore con parametri che permette di specificare la parte reale e immaginaria al momento dell'istanza 
+    complex_number(T r, T i) : real(r), imag(i) {}
+
+    // Metodi per ottenere parte reale, immaginaria e coniugato
+    T get_real() const { return real; }
+    T get_imag() const { return imag; }
+    complex_number conjugate() const { return complex_number(real, -imag); }
