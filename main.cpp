b2f5f42fab6c5ad5a219553ec050cb70202b1cc9
unknown <s283814@studenti.polito.it>
2025-03-31 19:01:33 +0200

    aggiunta int main()

--- main.cpp
+++ main.cpp
@@ -58,3 +58,25 @@ public:
         return os;
     }
 };
+//Test della classe
+int main() {
+    complex_number<double> n1(1, 2);
+    complex_number<double> n2(3, -4);
+
+    std::cout << "n1: " << n1 << std::endl;
+    std::cout << "n2: " << n2 << std::endl;
+
+    complex_number<double> n3 = n1 + n2;
+    std::cout << "n1 + n2: " << n3 << std::endl;
+
+    n1 += n2;
+    std::cout << "Dopo n1 += n2: " << n1 << std::endl;
+
+    complex_number<double> n4 = n1 * n2;
+    std::cout << "n1 * n2: " << n4 << std::endl;
+
+    n1 *= n2;
+    std::cout << "Dopo n1 *= n2: " << n1 << std::endl;
+
+    return 0;
+}
