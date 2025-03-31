5b583351b8b0db6a6a7af1d14db91e2bc23b0cd6
unknown <s283814@studenti.polito.it>
2025-03-31 15:42:15 +0200

    overload

--- main.cpp
+++ main.cpp
@@ -20,3 +20,41 @@ public:
     T get_real() const { return real; }
     T get_imag() const { return imag; }
     complex_number conjugate() const { return complex_number(real, -imag); }
+
+   // Overload dell'operatore +=
+    complex_number& operator+=(const complex_number& other) {
+        real += other.real;
+        imag += other.imag;
+        return *this;
+    }
+
+    // Overload dell'operatore +
+    complex_number operator+(const complex_number& other) const {
+        return complex_number(real + other.real, imag + other.imag);
+    }
+
+    // Overload dell'operatore *=
+    complex_number& operator*=(const complex_number& other) {
+        T new_real = real * other.real - imag * other.imag;
+        T new_imag = real * other.imag + imag * other.real;
+        real = new_real;
+        imag = new_imag;
+        return *this;
+    }
+
+    // Overload dell'operatore *
+    complex_number operator*(const complex_number& other) const {
+        return complex_number(real * other.real - imag * other.imag,
+                              real * other.imag + imag * other.real);
+    }
+
+    // Overload dell'operatore <<
+    friend std::ostream& operator<<(std::ostream& os, const complex_number& c) {
+        os << c.real;
+        if (c.imag >= 0)
+            os << "+" << c.imag << "i";
+        else
+            os << c.imag << "i";
+        return os;
+    }
+};
